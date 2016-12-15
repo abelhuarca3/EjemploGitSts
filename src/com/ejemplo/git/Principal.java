de888b3accba66b14504614f614176e8dfbae5f3
abelhuarca3 <abelhuarca3@gmail.com>
2016-12-15 14:59:20 -0500

    Segundo Commit

--- src/com/ejemplo/git/Principal.java
+++ src/com/ejemplo/git/Principal.java
@@ -1,5 +1,10 @@
 package com.ejemplo.git;
 
 public class Principal {
-
+	public static void main(String[] args) {
+		Suma suma = new Suma();
+		
+		
+		System.out.println(suma.sumar(4, 5));
+	}
 }
---
+++ src/com/ejemplo/git/Suma.java
@@ -0,0 +1,8 @@
+package com.ejemplo.git;
+
+public class Suma {
+    
+	public int sumar(int numero1, int numero2) {
+		return numero1+numero2;
+	}
+}
